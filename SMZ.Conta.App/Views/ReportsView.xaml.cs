d50d19711bf5b6433171865ad94b596da7038f46
alessiavittori <alessiavittori2001@gmail.com>
2026-06-08 15:08:56 +0200

    Estrae vista stampe e report

---
+++ SMZ.Conta.App/Views/ReportsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace SMZ.Conta.App.Views;
+
+public partial class ReportsView : UserControl
+{
+    public ReportsView()
+    {
+        InitializeComponent();
+    }
+}
