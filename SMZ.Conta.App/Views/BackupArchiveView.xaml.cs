6e81996ab478ae46ed3a04d351cc59b6895aef85
alessiavittori <alessiavittori2001@gmail.com>
2026-06-08 15:16:16 +0200

    Estrae vista backup e archivio

---
+++ SMZ.Conta.App/Views/BackupArchiveView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace SMZ.Conta.App.Views;
+
+public partial class BackupArchiveView : UserControl
+{
+    public BackupArchiveView()
+    {
+        InitializeComponent();
+    }
+}
